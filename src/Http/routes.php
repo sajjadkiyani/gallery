bc88c12a30f09dcab65b5625f6e6a2fddde294c2
sajjadkiyani <77837511+sajjadkiyani@users.noreply.github.com>
2021-01-23 19:16:00 +0330

    Update routes.php

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,5 +1,5 @@
 <?php
 use Kiyani\Gallery\Http\Controllers\GalleryController ;
-Route::group(['prefix' => 'dashboard'] ,function (){
+Route::group(['prefix' => 'dashboard' ,'middleware' => ['web']] ,function (){
     Route::resource('gallery',GalleryController::class);
 });
