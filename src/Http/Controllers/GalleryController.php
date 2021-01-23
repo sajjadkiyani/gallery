1f1b86bde7c86475b19c1cb190afd8d6f864e34d
sajjadkiyani <77837511+sajjadkiyani@users.noreply.github.com>
2021-01-23 20:31:31 +0330

    Update GalleryController.php

--- src/Http/Controllers/GalleryController.php
+++ src/Http/Controllers/GalleryController.php
@@ -3,6 +3,7 @@ namespace Kiyani\Gallery\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Kiyani\Gallery\Models\Gallery;
+use RealRashid\SweetAlert\Facades\Alert;
 class GalleryController extends BaseController
 {
 
@@ -27,10 +28,12 @@ class GalleryController extends BaseController
         ]);
         if ($gallery)
             $links = $this->uploadImage($request->images ,'/gallery/'.$gallery->id.'/');
-            foreach ($links as $link){
-                $gallery->images()->create([
-                    'url' => $link
-                ]);
+            if ($links){
+                foreach ($links as $link){
+                    $gallery->images()->create([
+                        'url' => $link
+                    ]);
+                }
             }
         return redirect()->route('gallery.index') ;
     }
@@ -43,11 +46,13 @@ class GalleryController extends BaseController
     public function edit($id)
     {
         $gallery = Gallery::whereId($id)->first();
+        Alert::success('Success Title', 'Success Message');
         return view('gallery::dashboard.gallery.edit',compact('gallery'));
     }
 
     public function update(Request $request,$id)
     {
+
         $this->validation($request);
         $gallery = Gallery::find($id);
         $gallery->update([
@@ -56,6 +61,8 @@ class GalleryController extends BaseController
             'is_private' => $request->private ,
         ]);
             $links = $this->uploadImage($request->images ,'/gallery/'.$gallery->id.'/');
+        SweetAlert::message('Robots are working!');
+        return redirect(route('gallery.index'));
         if ($links)
             foreach ($links as $link){
             $gallery->images()->create([
